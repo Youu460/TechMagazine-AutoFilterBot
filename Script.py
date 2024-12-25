d854fd48eda92eb75195e989c3562a3a2f8b2fd2
Youu460 <nisanox34@gmail.com>
2024-12-25 09:39:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - TechMagazine-AutoFilterBot is a open source project. 
-- Source - https://github.com/itsyogieu/TechMagazine-AutoFilterBot
+- Source - https://t.me/ISAGI_x3
 
 <b>DEVS:</b>
 - <a href=https://t.me/ISAGI_x3>ISAGI_x3</a>"""
